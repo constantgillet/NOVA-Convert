30210533f120efe9778d53a366fa6336bb70389e
constantgillet <constant.gillet@hetic.net>
2020-05-13 19:33:07 +0200

    add convert pound to kg

--- src/index.js
+++ src/index.js
@@ -34,6 +34,20 @@ class Convert extends LIBRARIES.Skill{
         }
       }
     });
+
+    //Convert pounds to kg
+    this.Main.Manager.addAction("convertPoundToKilogram.number", function(_intent, _socket){
+      if(_intent.Variables.number !== undefined){
+        if(!isNaN(parseFloat(_intent.Variables.number))){
+          
+          let number = _intent.Variables.number / 2.205;
+          number = number.toPrecision(4);
+          _intent.Variables.value = number;
+
+          _intent.answer(_socket);
+        }
+      }
+    });
   }
 }
 
